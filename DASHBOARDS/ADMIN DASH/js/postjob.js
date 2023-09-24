2daaa3ef6c6a4c17210936ed1e018cc1c032bc7a
anneeeng <rachellanneviray16@gmail.com>
2023-09-24 21:32:01 +0800

    admin

--- DASHBOARDS/ADMIN DASH/js/postjob.js
+++ DASHBOARDS/ADMIN DASH/js/postjob.js
@@ -55,16 +55,40 @@ const jobs = [
   ];
   
   const jobsHeading = document.querySelector(".jobs-list-container h2");
-  const jobsContainer = document.querySelector(".jobs-list-container .jobs");
+const jobsContainer = document.querySelector(".jobs-list-container .jobs");
   const jobSearch = document.querySelector(".jobs-list-container .job-search");
   const postJobButton = document.getElementById('postJobButton');
-
-  postJobButton.addEventListener('click', () => {
-    // Display the post job form
-    const postJobForm = document.getElementById('postJobForm');
-    postJobForm.style.display = 'block';
-  });
+  const postJobForm = document.getElementById('job-form'); // Changed the ID to match the form
   
+ // Add a click event listener to the "Post a Job" button
+postJobButton.addEventListener('click', () => {
+  // Display the post job form
+  postJobForm.style.display = 'block';
+});
+// Add a submit event listener to the job posting form
+postJobForm.addEventListener('submit', (e) => {
+  e.preventDefault(); // Prevent the form from submitting
+
+  // Here, you can access form fields and their values
+  const jobTitle = document.getElementById('jobTitle').value;
+  const jobDetails = document.getElementById('jobDetails').value;
+
+  // Create a new job object and add it to the jobs array
+  const newJob = {
+    title: jobTitle,
+    details: jobDetails,
+    // Add other properties as needed
+  };
+
+  jobs.push(newJob);
+
+  // Hide the form after submission
+  postJobForm.style.display = 'none';
+
+  // Re-render the job listings with the new job
+  createJobListingCards();
+});
+
   let searchTerm = "";
   
   if (jobs.length == 1) {
@@ -116,23 +140,112 @@ function handleDelete(jobCard, jobToDelete) {
         deleteIcon.alt = "Delete";
         deleteIcon.classList.add("delete-icon");
 
-      // Add a click event listener to the delete icon
+        // Add a click event listener to the delete icon
         deleteIcon.addEventListener("click", () => {
         // Handle the delete action here
         handleDelete(jobCard, job); // Pass the job card element and job object to the handleDelete function
         });
 
+        // Function to handle the delete action
+        function handleDelete(jobCard, jobToDelete) {
+        // Remove the job card from the DOM
+        jobCard.remove();
+
+  // Find the index of the job in the jobs array based on the jobToDelete object
+  const indexToDelete = jobs.findIndex((job) => job === jobToDelete);
+
+  if (indexToDelete !== -1) {
+    // Remove the job from the jobs array
+    jobs.splice(indexToDelete, 1);
+  }
+}
+
+// Function to handle the edit action
+function handleEdit(job) {
+  // For simplicity, let's open an alert with the job title as a placeholder.
+  const newJobTitle = prompt("Edit Job Title:", job.title);
+
+  if (newJobTitle !== null) {
+    // Update the job title in the jobs array
+    job.title = newJobTitle;
+
+    // Update the job card in the DOM
+    const jobCard = document.querySelector(`.job-title:contains("${job.title}")`);
+    if (jobCard) {
+      jobCard.innerHTML = newJobTitle;
+    }
+  }
+}
+// Add a click event listener to the "Post a Job" button
+postJobButton.addEventListener('click', () => {
+  // Display an alert window with your custom content
+  const alertContent = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="newjob.css">
+        <title>Admin Post Job</title>
+    </head>
+    <body>
+        <h1>Post a Job</h1>
+        <form id="job-form">
+            <label for="job-title">Job Title:</label>
+            <input type="text" id="job-title" name="job-title" required>
+
+            <label for="job-description">Job Description:</label>
+            <textarea id="job-description" name="job-description" required></textarea>
+
+            <label for="job-location">Job Location:</label>
+            <input type="text" id="job-location" name="job-location" required>
+
+            <label for="job-type">Job Type:</label>
+            <select id="job-type" name="job-type" required>
+                <option value="full-time">Full-time</option>
+                <option value="part-time">Part-time</option>
+                <option value="contract">Contract</option>
+            </select>
+
+            <label for="job-salary">Job Salary:</label>
+            <input type="text" id="job-salary" name="job-salary" required>
+
+            <button type="submit">Post Job</button>
+        </form>
+        
+        <div id="job-link" style="display: none;">
+            <p>Job successfully posted! Here's the link:</p>
+            <a id="job-url" href="#" target="_blank"></a>
+        </div>
+    </body>
+    </html>
+  `;
+
+  // Show the alert
+  showAlert(alertContent);
+});
+
+// Function to display the alert
+function showAlert(content) {
+  // Create a modal dialog
+  const modal = document.createElement('div');
+  modal.className = 'modal';
+  modal.innerHTML = content;
+
+  // Display the modal dialog
+  document.body.appendChild(modal);
+}
+
         let editIcon = document.createElement("img");
         editIcon.src = "img/edit.svg"; // Replace with the actual path to your edit SVG icon
         editIcon.alt = "Edit";
         editIcon.classList.add("edit-icon");
-  
+
         // Add a click event listener to the edit icon
         editIcon.addEventListener("click", () => {
-          // Handle the edit action here
-          handleEdit(job);
+        // Handle the edit action here
+        handleEdit(job);
         });
-
 
         let openPositions = document.createElement("span");
         openPositions.classList.add("open-positions");
