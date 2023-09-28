636fdd92cc2337f7a073512a7e7991096928a330
Hear-AYA <mendozajanine623@gmail.com>
2023-09-28 11:15:17 +0800

    latest

--- DASHBOARDS/ADMIN DASH/js/postjob.js
+++ DASHBOARDS/ADMIN DASH/js/postjob.js
@@ -1,55 +1,50 @@
 const jobs = [
   {
-    title: "Software Engineer",
-    image: "img/software-engineer.svg",
+    title: "Instructor III",
     details:
-      "Responsible for designing, developing and maintaining software systems and applications.",
+      "Gives lessons that allow students to increase their knowledge of a specific subject. By being proficient at job duties, help those they instruct prepare for future opportunities, such as gaining additional education or performing well in their chosen career.",
     openPositions: "2",
     link: "#",
   },
 
   {
-    title: "Data Scientist",
-    image: "img/data-scientist.svg",
+    title: "Assistant Professor I",
     details:
-      "Responsible for collecting, analyzing and interpreting large data sets to help organizations make better decisions.",
+      "Responsible for delivering lectures and coordinating other departmental activities. Should be able to offer academic support to students when required. Should be able to attend various conferences and seminars. Also be able to prepare presentations and deliver guest lectures.",
     openPositions: "3",
     link: "#",
   },
 
   {
-    title: "Project Manager",
-    image: "img/project-manager.svg",
+    title: "Assistant Professor III",
     details:
-      "Responsible for planning, executing and closing projects on time and within budget.",
+      "Should display excellent academic background. In addition to this, should also be able to address students’ questions and queries. A successful candidate should be well-versed with the educational policies and any changes in the field of education.",
+    openPositions: "1",
+    link: "#",
+  },
+
+
+  {
+    title: "Project Manager Assistant Professor III",
+    details:
+      "Should display excellent academic background. In addition to this, should also be able to address students’ questions and queries. A successful candidate should be well-versed with the educational policies and any changes in the field of education. ",
     openPositions: "1",
     link: "#",
   },
 
   {
-    title: "Product Manager",
-    image: "img/product-manager.svg",
+    title: "City Government Assistant Department Head III",
     details:
-      "Responsible for managing the entire product life cycle, from ideation to launch and post-launch maintenance.",
+      "Duties include managing inquiries and complaints, developing strategic solutions, satisfaction, and identifying opportunities. Plan, direct, supervises, and coordinates activities/operations in the department. Education. Bachelor's Degree. Experience.",
     openPositions: "1",
     link: "#",
   },
 
   {
-    title: "Sales Representative",
-    image: "img/sales-representative.svg",
+    title: "Sales Representative Administrative Assistant I ( Secretary I)",
     details:
-      "Responsible for reaching out to potential customers and closing sales deals.",
+      "Arranging and scheduling appointments, meetings, and events. Monitoring office supplies and ordering replacements. Assisting with copying, scanning, faxing, emailing, note-taking, and travel bookings. Preparing facilities and arranging refreshments for events, if required.",
     openPositions: "4",
-    link: "#",
-  },
-
-  {
-    title: "Marketing Manager",
-    image: "img/marketing-manager.svg",
-    details:
-      "Responsible for creating and executing marketing strategies to promote a company or product.",
-    openPositions: "1",
     link: "#",
   },
 ];
@@ -97,91 +92,30 @@ const positionTitleInput = createFormInput(
   job.title
 );
 
+const salaryInput = createFormInput(
+  "Monthly Salary",
+  "text",
+  job.monthlySalary || ""
+);
+
+const qualificationsInput = createFormInput(
+  "Qualifications Standard",
+  "text",
+  job.qualifications|| ""
+);
+
 const requirementsInput = createFormInput(
   "Requirements",
   "text",
   job.requirements || ""
 );
 
-const salaryInput = createFormInput(
-  "Salary/Job/Pay Grade",
-  "text",
-  job.salary || ""
-);
-
-const monthlySalaryInput = createFormInput(
-  "Monthly Salary",
-  "text",
-  job.monthlySalary || ""
-);
-
-// Qualifications section
-const qualificationsFieldset = document.createElement("fieldset");
-qualificationsFieldset.innerHTML = "<legend>Qualifications</legend>";
-
-const educationInput = createFormInput(
-  "Education",
-  "text",
-  job.qualifications?.education || ""
-);
-
-const trainingInput = createFormInput(
-  "Training",
-  "text",
-  job.qualifications?.training || ""
-);
-
-const experienceInput = createFormInput(
-  "Experience",
-  "text",
-  job.qualifications?.experience || ""
-);
-
-const eligibilityInput = createFormInput(
-  "Eligibility",
-  "text",
-  job.qualifications?.eligibility || ""
-);
-
-const researchOutputInput = createFormInput(
-  "Research Output",
-  "text",
-  job.qualifications?.researchOutput || ""
-);
-
-const communityExtensionServiceInput = createFormInput(
-  "Community Extension Service",
-  "text",
-  job.qualifications?.communityExtensionService || ""
-);
-
-const competencyInput = createFormInput(
-  "Competency (if applicable)",
-  "text",
-  job.qualifications?.competency || ""
-);
-
-const placeOfAssignmentInput = createFormInput(
-  "Place of Assignment",
-  "text",
-  job.qualifications?.placeOfAssignment || ""
-);
-
 // Append the form fields to the form
 editForm.appendChild(positionTitleInput);
-editForm.appendChild(requirementsInput);
 editForm.appendChild(salaryInput);
-editForm.appendChild(monthlySalaryInput);
+editForm.appendChild(qualificationsInput);
+editForm.appendChild(requirementsInput);
 
-qualificationsFieldset.appendChild(educationInput);
-qualificationsFieldset.appendChild(trainingInput);
-qualificationsFieldset.appendChild(experienceInput);
-qualificationsFieldset.appendChild(eligibilityInput);
-qualificationsFieldset.appendChild(researchOutputInput);
-qualificationsFieldset.appendChild(communityExtensionServiceInput);
-qualificationsFieldset.appendChild(competencyInput);
-qualificationsFieldset.appendChild(placeOfAssignmentInput);
-editForm.appendChild(qualificationsFieldset);
 
 // Add a submit button for the form
 const submitButton = document.createElement("button");
@@ -191,21 +125,10 @@ submitButton.addEventListener("click", (e) => {
 
   // Update the job details based on the form inputs
   job.title = positionTitleInput.querySelector("input").value;
+  job.monthlySalary = salaryInput.querySelector("input").value;
+  job.qualifications = qualificationsInput.querySelector("input").value;
   job.requirements = requirementsInput.querySelector("input").value;
-  job.salary = salaryInput.querySelector("input").value;
-  job.monthlySalary = monthlySalaryInput.querySelector("input").value;
   
-  job.qualifications = {
-    education: educationInput.querySelector("input").value,
-    training: trainingInput.querySelector("input").value,
-    experience: experienceInput.querySelector("input").value,
-    eligibility: eligibilityInput.querySelector("input").value,
-    researchOutput: researchOutputInput.querySelector("input").value,
-    communityExtensionService: communityExtensionServiceInput.querySelector("input").value,
-    competency: competencyInput.querySelector("input").value,
-    placeOfAssignment: placeOfAssignmentInput.querySelector("input").value,
-  };
-
   // Close the edit form and update the job card
   editForm.remove();
   createJobListingCards();
@@ -249,75 +172,79 @@ if (indexToDelete !== -1) {
 }
 
 function createJobListingCards() {
-  jobsContainer.innerHTML = "";
+jobsContainer.innerHTML = "";
 
-  jobs.forEach((job) => {
-    if (job.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-      let jobCard = document.createElement("div");
-      jobCard.classList.add("job");
+jobs.forEach((job) => {
+  if (job.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+    let jobCard = document.createElement("div");
+    jobCard.classList.add("job");
 
-      let buttonContainer = document.createElement("div"); // Create a container for buttons
-      buttonContainer.classList.add("button-container");
+    let title = document.createElement("h3");
+    title.innerHTML = job.title;
+    title.classList.add("job-title");
 
-      let editIcon = document.createElement("img");
-      editIcon.src = "img/edit.svg"; // Replace with the actual path to your edit SVG icon
-      editIcon.alt = "Edit";
-      editIcon.classList.add("edit-icon");
+    let details = document.createElement("div");
+    details.innerHTML = job.details;
+    details.classList.add("details");
 
-      // Add a click event listener to the edit icon
-      editIcon.addEventListener("click", () => {
-        // Handle the edit action here
-        handleEdit(job);
-      });
+    let detailsBtn = document.createElement("a");
+    detailsBtn.href = job.link;
+    detailsBtn.textContent = "More Details";
+    detailsBtn.classList.add("details-btn");
 
-      let deleteIcon = document.createElement("img");
-      deleteIcon.src = "img/delete.svg"; // Replace with the actual path to your delete SVG icon
-      deleteIcon.alt = "Delete";
-      deleteIcon.classList.add("delete-icon");
+    let editIcon = document.createElement("img");
+    editIcon.src = "img/edit.svg"; // Replace with the actual path to your edit SVG icon
+    editIcon.alt = "Edit";
+    editIcon.classList.add("edit-icon");
 
-      // Add a click event listener to the delete icon
-      deleteIcon.addEventListener("click", () => {
-        // Handle the delete action here
-        handleDelete(jobCard, job);
-      });
-
-      buttonContainer.appendChild(editIcon); // Add the edit icon to the button container
-      buttonContainer.appendChild(deleteIcon); // Add the delete icon to the button container
-
-      let title = document.createElement("h3");
-      title.innerHTML = job.title;
-      title.classList.add("job-title");
-
-      let details = document.createElement("div");
-      details.innerHTML = job.details;
-      details.classList.add("details");
-
-      let detailsBtn = document.createElement("a");
-      detailsBtn.href = job.link;
-      detailsBtn.textContent = "More Details";
-      detailsBtn.classList.add("details-btn");
-
-      let openPositions = document.createElement("span");
-      openPositions.classList.add("open-positions");
-
-      if (job.openPositions == 1) {
-        openPositions.innerHTML = `${job.openPositions} open position`;
-      } else {
-        openPositions.innerHTML = `${job.openPositions} open positions`;
-      }
-
-      jobCard.appendChild(buttonContainer); // Add the button container to the job card
-      jobCard.appendChild(title);
-      jobCard.appendChild(details);
-      jobCard.appendChild(detailsBtn);
-      jobCard.appendChild(openPositions);
-
-      jobsContainer.appendChild(jobCard);
-    }
-  });
+// Updated code for edit icon click event
+jobsContainer.addEventListener("click", (event) => {
+const editButton = event.target.closest(".edit-icon");
+if (editButton) {
+  event.preventDefault(); // Prevent the click event from propagating
+  console.log("Edit button clicked"); // Add this line for debugging
+  const jobCard = editButton.closest(".job");
+  const jobTitle = jobCard.querySelector(".job-title").textContent;
+  // Find the corresponding job object by title
+  const jobObject = jobs.find((job) => job.title === jobTitle);
+  if (jobObject) {
+    handleEdit(jobObject); // Pass the entire job object to the handleEdit function
+  }
 }
+});
 
 
+    let deleteIcon = document.createElement("img");
+    deleteIcon.src = "img/delete.svg"; // Replace with the actual path to your delete SVG icon
+    deleteIcon.alt = "Delete";
+    deleteIcon.classList.add("delete-icon");
+
+    // Add a click event listener to the delete icon
+    deleteIcon.addEventListener("click", () => {
+      // Handle the delete action here
+      handleDelete(jobCard, job); // Pass the job card element and job object to the handleDelete function
+    });
+
+    let openPositions = document.createElement("span");
+    openPositions.classList.add("open-positions");
+
+    if (job.openPositions == 1) {
+      openPositions.innerHTML = `${job.openPositions} open position`;
+    } else {
+      openPositions.innerHTML = `${job.openPositions} open positions`;
+    }
+
+    jobCard.appendChild(title);
+    jobCard.appendChild(details);
+    jobCard.appendChild(detailsBtn);
+    jobCard.appendChild(deleteIcon); // Add the delete icon
+    jobCard.appendChild(editIcon); // Add the edit icon
+    jobCard.appendChild(openPositions);
+
+    jobsContainer.appendChild(jobCard);
+  }
+});
+}
 
 createJobListingCards();
 
--- DASHBOARDS/CLIENT DASH/js/job.js
+++ DASHBOARDS/CLIENT DASH/js/job.js
@@ -1,58 +1,54 @@
 const jobs = [
-    {
-      title: "Software Engineer",
-      details:
-        "Responsible for designing, developing and maintaining software systems and applications.",
-      openPositions: "2",
-      link: "#",
-    },
-  
-    {
-      title: "Data Scientist",
-      image: "img/data-scientist.svg",
-      details:
-        "Responsible for collecting, analyzing and interpreting large data sets to help organizations make better decisions.",
-      openPositions: "3",
-      link: "#",
-    },
-  
-    {
-      title: "Project Manager",
-      image: "img/project-manager.svg",
-      details:
-        "Responsible for planning, executing and closing projects on time and within budget.",
-      openPositions: "1",
-      link: "#",
-    },
-  
-    {
-      title: "Product Manager",
-      image: "img/product-manager.svg",
-      details:
-        "Responsible for managing the entire product life cycle, from ideation to launch and post-launch maintenance.",
-      openPositions: "1",
-      link: "#",
-    },
-  
-    {
-      title: "Sales Representative",
-      image: "img/sales-representative.svg",
-      details:
-        "Responsible for reaching out to potential customers and closing sales deals.",
-      openPositions: "4",
-      link: "#",
-    },
-  
-    {
-      title: "Marketing Manager",
-      image: "img/marketing-manager.svg",
-      details:
-        "Responsible for creating and executing marketing strategies to promote a company or product.",
-      openPositions: "1",
-      link: "#",
-    },
-  ];
-  
+  {
+    title: "Instructor III",
+    details:
+      "Gives lessons that allow students to increase their knowledge of a specific subject. By being proficient at job duties, help those they instruct prepare for future opportunities, such as gaining additional education or performing well in their chosen career.",
+    openPositions: "2",
+    link: "#",
+  },
+
+  {
+    title: "Assistant Professor I",
+    details:
+      "Responsible for delivering lectures and coordinating other departmental activities. Should be able to offer academic support to students when required. Should be able to attend various conferences and seminars. Also be able to prepare presentations and deliver guest lectures.",
+    openPositions: "3",
+    link: "#",
+  },
+
+  {
+    title: "Assistant Professor III",
+    details:
+      "Should display excellent academic background. In addition to this, should also be able to address students’ questions and queries. A successful candidate should be well-versed with the educational policies and any changes in the field of education.",
+    openPositions: "1",
+    link: "#",
+  },
+
+
+  {
+    title: "Project Manager Assistant Professor III",
+    details:
+      "Should display excellent academic background. In addition to this, should also be able to address students’ questions and queries. A successful candidate should be well-versed with the educational policies and any changes in the field of education. ",
+    openPositions: "1",
+    link: "#",
+  },
+
+  {
+    title: "City Government Assistant Department Head III",
+    details:
+      "Duties include managing inquiries and complaints, developing strategic solutions, satisfaction, and identifying opportunities. Plan, direct, supervises, and coordinates activities/operations in the department. Education. Bachelor's Degree. Experience.",
+    openPositions: "1",
+    link: "#",
+  },
+
+  {
+    title: "Sales Representative Administrative Assistant I ( Secretary I)",
+    details:
+      "Arranging and scheduling appointments, meetings, and events. Monitoring office supplies and ordering replacements. Assisting with copying, scanning, faxing, emailing, note-taking, and travel bookings. Preparing facilities and arranging refreshments for events, if required.",
+    openPositions: "4",
+    link: "#",
+  },
+];
+
   const jobsHeading = document.querySelector(".jobs-list-container h2");
   const jobsContainer = document.querySelector(".jobs-list-container .jobs");
   const jobSearch = document.querySelector(".jobs-list-container .job-search");
