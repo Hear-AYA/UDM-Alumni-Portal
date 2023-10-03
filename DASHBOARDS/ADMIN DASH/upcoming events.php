60367e36d2e44acb916347c1a142ec63ecfc318e
Hear-AYA <mendozajanine623@gmail.com>
2023-10-03 21:20:27 +0800

    22 event

--- DASHBOARDS/ADMIN DASH/upcoming events.php
+++ DASHBOARDS/ADMIN DASH/upcoming events.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
-    <link rel="stylesheet" href="css/user dash.css">
-    <link rel="stylesheet" href="css/announcement.css">
+    <link rel="stylesheet" href="css/admin dash.css">
+    <link rel="stylesheet" href="css/upcoming events.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
