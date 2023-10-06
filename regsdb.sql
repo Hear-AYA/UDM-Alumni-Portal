f48ffc951d900ac63dbf83584fc3b2d861e64b93
avendano008 <87066382+avendano008@users.noreply.github.com>
2023-10-06 12:40:42 +0800

    Create regsdb.sql

---
+++ regsdb.sql
@@ -0,0 +1,161 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 06, 2023 at 06:40 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `regsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job`
+--
+
+CREATE TABLE `job` (
+  `id` int(20) NOT NULL,
+  `position_title` varchar(500) NOT NULL,
+  `requirements` varchar(500) NOT NULL,
+  `job` varchar(500) NOT NULL,
+  `monthly_salary` varchar(500) NOT NULL,
+  `education` varchar(500) NOT NULL,
+  `training` varchar(500) NOT NULL,
+  `experience` varchar(500) NOT NULL,
+  `eligibility` varchar(500) NOT NULL,
+  `research` varchar(500) NOT NULL,
+  `community` varchar(500) NOT NULL,
+  `competency` varchar(500) NOT NULL,
+  `assignment` varchar(500) NOT NULL,
+  `open_positions` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `job`
+--
+
+INSERT INTO `job` (`id`, `position_title`, `requirements`, `job`, `monthly_salary`, `education`, `training`, `experience`, `eligibility`, `research`, `community`, `competency`, `assignment`, `open_positions`) VALUES
+(1, 'test1', 'Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem ', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test2', '2'),
+(2, 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', '4'),
+(3, 'test4', 'test3 test3test3 test3 test3test3', 'test3', 'test3', 'test3', 'test3', 'test3', 'test3', 'test3', 'test3', 'test3', 'test3', '5');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `professional`
+--
+
+CREATE TABLE `professional` (
+  `id` int(250) NOT NULL,
+  `alumni_id` varchar(500) NOT NULL,
+  `rad1` varchar(500) NOT NULL,
+  `rad2` varchar(500) NOT NULL,
+  `rad3` varchar(500) NOT NULL,
+  `rad4` varchar(500) NOT NULL,
+  `rad5` varchar(500) NOT NULL,
+  `rad6` varchar(500) NOT NULL,
+  `rad7` varchar(500) NOT NULL,
+  `rad8` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `professional`
+--
+
+INSERT INTO `professional` (`id`, `alumni_id`, `rad1`, `rad2`, `rad3`, `rad4`, `rad5`, `rad6`, `rad7`, `rad8`) VALUES
+(2, '12 ', '1', '0', '1', '1', '0', '1', '0', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `regsinfo`
+--
+
+CREATE TABLE `regsinfo` (
+  `id` int(11) NOT NULL,
+  `fullName` varchar(100) NOT NULL,
+  `img_loc` varchar(500) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `pass` varchar(100) NOT NULL,
+  `cpass` varchar(100) NOT NULL,
+  `pnum` bigint(10) NOT NULL,
+  `course` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `award` varchar(250) NOT NULL,
+  `year_graduated` varchar(250) NOT NULL,
+  `current_job` varchar(250) NOT NULL,
+  `job_experience` varchar(250) NOT NULL,
+  `skill_expertise` varchar(250) NOT NULL,
+  `dateofbirth` varchar(250) NOT NULL,
+  `degree` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `regsinfo`
+--
+
+INSERT INTO `regsinfo` (`id`, `fullName`, `img_loc`, `email`, `pass`, `cpass`, `pnum`, `course`, `gender`, `award`, `year_graduated`, `current_job`, `job_experience`, `skill_expertise`, `dateofbirth`, `degree`) VALUES
+(12, 'Alexander Avendano', '1696529040_355721356_274600031814272_1868331215881352822_n.png', 'a.avendano008@gmail.com', 'admin', 'admin', 9558456111, 'BSIT', 'Female', 'test', 'test', 'test', 'test', 'test', '2023-10-04', 'test');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `job`
+--
+ALTER TABLE `job`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `professional`
+--
+ALTER TABLE `professional`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `regsinfo`
+--
+ALTER TABLE `regsinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `job`
+--
+ALTER TABLE `job`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `professional`
+--
+ALTER TABLE `professional`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `regsinfo`
+--
+ALTER TABLE `regsinfo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
