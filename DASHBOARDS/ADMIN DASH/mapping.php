376722f7a646d37e02288e9aff2a76caafced99e
avendano008 <87066382+avendano008@users.noreply.github.com>
2023-11-21 12:53:41 +0800

    Update mapping.php

--- DASHBOARDS/ADMIN DASH/mapping.php
+++ DASHBOARDS/ADMIN DASH/mapping.php
@@ -48,17 +48,13 @@
                   $rad2="0";
                   $rad3="0";
                   $rad4="0";
-                  $rad5="0";
-                  $rad6="0";
-                  $rad7="0";
-                  $rad8="0";
                 }
 
 
               ?>
               <script>
-              const xValues = ['Q 1','Q 2','Q 3','Q 4','Q 5','Q 6','Q 7','Q 8'];
-              const yValues = ['<?php echo$rad1?>','<?php echo$rad2?>','<?php echo$rad3?>','<?php echo$rad4?>','<?php echo$rad5?>','<?php echo$rad6?>','<?php echo$rad7?>','<?php echo$rad8?>'];
+              const xValues = ['Q 1','Q 2','Q 3','Q 4'];
+              const yValues = ['<?php echo$rad1?>','<?php echo$rad2?>','<?php echo$rad3?>','<?php echo$rad4?>'];
 
               new Chart("myChart", {
                 type: "line",
@@ -81,7 +77,250 @@
               });
               </script>
             </div>
+            <div class="row">
+              <div class="col-md-6">
+                <canvas id="myChart1" style="width:100%;max-width:600px"></canvas>
+                <?php
+
+                $data_score="0";
+
+                $sql = "SELECT COUNT(rad1) FROM professional WHERE rad1='1'";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                  // output data of each row
+                  while($row = $result->fetch_assoc()) {
+                    $data_score=$row['COUNT(rad1)'];
+                  }
+                } else {
+                  $data_score="0";
+                }
+                ?>
+
+                 <?php
+
+                $data_score1="0";
+
+                $sql = "SELECT COUNT(rad1) FROM professional WHERE rad1='0'";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                  // output data of each row
+                  while($row = $result->fetch_assoc()) {
+                    $data_score1=$row['COUNT(rad1)'];
+                  }
+                } else {
+                  $data_score1="0";
+                }
+                ?>
+                <script>
+                var xValues1 = ["Yes", "No"];
+                var yValues1 = [<?php echo$data_score?>, <?php echo$data_score1?>, 44, 24, 15];
+                var barColors = ["blue", "blue"];
+
+                new Chart("myChart1", {
+                  type: "bar",
+                  data: {
+                    labels: xValues1,
+                    datasets: [{
+                      backgroundColor: barColors,
+                      data: yValues1
+                    }]
+                  },
+                  options: {
+                    legend: {display: false},
+                    title: {
+                      display: true,
+                      text: "Question 1"
+                    }
+                  }
+                });
+                </script>
+              </div>
+              <div class="col-md-6">
+                <canvas id="myChart2" style="width:100%;max-width:600px"></canvas>
+                <?php
+
+                $data_score="0";
+
+                $sql = "SELECT COUNT(rad2) FROM professional WHERE rad2='1'";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                  // output data of each row
+                  while($row = $result->fetch_assoc()) {
+                    $data_score=$row['COUNT(rad2)'];
+                  }
+                } else {
+                  $data_score="0";
+                }
+                ?>
+
+                 <?php
+
+                $data_score1="0";
+
+                $sql = "SELECT COUNT(rad2) FROM professional WHERE rad2='0'";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                  // output data of each row
+                  while($row = $result->fetch_assoc()) {
+                    $data_score1=$row['COUNT(rad2)'];
+                  }
+                } else {
+                  $data_score1="0";
+                }
+                ?>
+                <script>
+                var xValues2 = ["Yes", "No"];
+                var yValues2 = [<?php echo$data_score?>, <?php echo$data_score1?>, 44, 24, 15];
+                var barColors = ["blue", "blue"];
+
+                new Chart("myChart2", {
+                  type: "bar",
+                  data: {
+                    labels: xValues2,
+                    datasets: [{
+                      backgroundColor: barColors,
+                      data: yValues2
+                    }]
+                  },
+                  options: {
+                    legend: {display: false},
+                    title: {
+                      display: true,
+                      text: "Question 2"
+                    }
+                  }
+                });
+                </script>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <canvas id="myChart3" style="width:100%;max-width:600px"></canvas>
+                <?php
+
+                $data_score="0";
+
+                $sql = "SELECT COUNT(rad3) FROM professional WHERE rad3='1'";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                  // output data of each row
+                  while($row = $result->fetch_assoc()) {
+                    $data_score=$row['COUNT(rad3)'];
+                  }
+                } else {
+                  $data_score="0";
+                }
+                ?>
+
+                 <?php
+
+                $data_score1="0";
+
+                $sql = "SELECT COUNT(rad3) FROM professional WHERE rad3='0'";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                  // output data of each row
+                  while($row = $result->fetch_assoc()) {
+                    $data_score1=$row['COUNT(rad3)'];
+                  }
+                } else {
+                  $data_score1="0";
+                }
+                ?>
+                <script>
+                var xValues3 = ["Yes", "No"];
+                var yValues3 = [<?php echo$data_score?>, <?php echo$data_score1?>, 44, 24, 15];
+                var barColors = ["blue", "blue"];
+
+                new Chart("myChart3", {
+                  type: "bar",
+                  data: {
+                    labels: xValues3,
+                    datasets: [{
+                      backgroundColor: barColors,
+                      data: yValues3
+                    }]
+                  },
+                  options: {
+                    legend: {display: false},
+                    title: {
+                      display: true,
+                      text: "Question 3"
+                    }
+                  }
+                });
+                </script>
+              </div>
+              <div class="col-md-6">
+                <canvas id="myChart4" style="width:100%;max-width:600px"></canvas>
+                <?php
+
+                $data_score="0";
+
+                $sql = "SELECT COUNT(rad4) FROM professional WHERE rad4='1'";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                  // output data of each row
+                  while($row = $result->fetch_assoc()) {
+                    $data_score=$row['COUNT(rad4)'];
+                  }
+                } else {
+                  $data_score="0";
+                }
+                ?>
+
+                 <?php
+
+                $data_score1="0";
+
+                $sql = "SELECT COUNT(rad4) FROM professional WHERE rad4='0'";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                  // output data of each row
+                  while($row = $result->fetch_assoc()) {
+                    $data_score1=$row['COUNT(rad4)'];
+                  }
+                } else {
+                  $data_score1="0";
+                }
+                ?>
+                <script>
+                var xValues4 = ["Yes", "No"];
+                var yValues4 = [<?php echo$data_score?>, <?php echo$data_score1?>, 44, 24, 15];
+                var barColors = ["blue", "blue"];
+
+                new Chart("myChart4", {
+                  type: "bar",
+                  data: {
+                    labels: xValues4,
+                    datasets: [{
+                      backgroundColor: barColors,
+                      data: yValues4
+                    }]
+                  },
+                  options: {
+                    legend: {display: false},
+                    title: {
+                      display: true,
+                      text: "Question 2"
+                    }
+                  }
+                });
+                </script>
+              </div>
+            </div>
           </div>
+          
+          </div>
+          
           
         </body>
         </html>
