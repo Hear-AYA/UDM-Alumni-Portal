6e77201e23ac388d18112916d026e193f68f2c73
avendano008 <87066382+avendano008@users.noreply.github.com>
2023-11-21 20:55:28 +0800

    Update mapping.php

--- DASHBOARDS/ADMIN DASH/mapping.php
+++ DASHBOARDS/ADMIN DASH/mapping.php
@@ -63,8 +63,8 @@
                   datasets: [{
                     fill: false,
                     lineTension: 0,
-                    backgroundColor: "rgba(0,0,255,1.0)",
-                    borderColor: "rgba(0,0,255,0.1)",
+                    backgroundColor: "black",
+                    borderColor: "black",
                     data: yValues
                   }]
                 },
